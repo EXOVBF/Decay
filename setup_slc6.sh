f57494be18e41ceddf55939e28e9cb1fb1a3c0d4
Luca Brianza <lucabrianza@gmail.com>
2014-02-27 17:32:53 +0100

    setup script for slc6

---
+++ setup_slc6.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.6/x86_64-slc6-gcc46-opt/setup.sh 
+export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/MCGenerators_lcgcmt66/pythia8/183/x86_64-slc6-gcc46-opt/lib/:$LD_LIBRARY_PATH
+export PATH=/afs/cern.ch/sw/lcg/external/MCGenerators_lcgcmt66/pythia8/183/x86_64-slc6-gcc46-opt/bin/:$PATH
+export PYTHIA8DATA=/afs/cern.ch/sw/lcg/external/MCGenerators_lcgcmt66/pythia8/183/x86_64-slc6-gcc46-opt/xmldoc
