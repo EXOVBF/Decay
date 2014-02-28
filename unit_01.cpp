23320bb5325972e722ce7ad6884d145cb6744198
Luca Brianza <lucabrianza@gmail.com>
2014-02-28 11:30:10 +0100

    Pythia_Hadronization only

---
+++ unit_01.cpp
@@ -0,0 +1,89 @@
+//****************************************************************************************************************************
+
+// on slc6: compile with -----> c++ -O2 -ansi -pedantic -W -Wall -I /afs/cern.ch/sw/lcg/external/HepMC/2.06.08/x86_64-slc6-gcc46-opt/include/ -L /afs/cern.ch/sw/lcg/external/HepMC/2.06.08/x86_64-slc6-gcc46-opt/lib -lHepMC -lpythia8tohepmc `pythia8-config --cxxflags --libs --ldflags` -o unit_01.exe unit_01.cpp
+
+//****************************************************************************************************************************
+
+// just for back-compatibility
+#ifdef PYTHIA8176
+#include "Pythia.h"
+#include "HepMCInterface.h"
+#define HepMCConverter HepMC::I_Pythia8
+#else
+#include "Pythia8/Pythia.h"
+#include "Pythia8/Pythia8ToHepMC.h"
+#define HepMCConverter HepMC::Pythia8ToHepMC
+#endif
+
+#include "HepMC/GenEvent.h"
+#include "HepMC/IO_GenEvent.h"
+#include "HepMC/IO_AsciiParticles.h"
+
+using namespace Pythia8; 
+
+int main(int argc, char **argv) 
+{
+    if (argc < 3) 
+    {
+        std::cerr << " Not enough input information!  no input file!" << std::endl;
+        exit (0);
+    }
+ 
+    std::string namefile_in;
+    namefile_in = argv[1];
+    std::string namefile_out;
+    namefile_out = argv[2];
+    std::string namefile_easy_out = "human_";
+    namefile_easy_out += (namefile_out);
+    
+
+	// Initialize Les Houches Event File run. List initialization information.
+	Pythia pythia;                           
+    std::string sfile = "Beams:LHEF ="+namefile_in;    
+    pythia.readString("Beams:frameType = 4");
+   	pythia.readString("HadronLevel:Hadronize = on");
+	pythia.readString(sfile.c_str());      
+    pythia.init();
+
+    // Interface for conversion from Pythia8::Event to HepMC event. 
+    //HepMC::I_Pythia8 ToHepMC;
+    HepMCConverter ToHepMC;
+    // Specify file where HepMC events will be stored.
+    HepMC::IO_GenEvent hepmc_file_out(namefile_out.c_str(), std::ios::out);
+    // Alternative output format -----> just for test 
+//    HepMC::IO_AsciiParticles human_file_out(namefile_easy_out.c_str(), std::ios::out);
+
+	// Allow for possibility of a few faulty events.
+	int nAbort = 10;
+	int iAbort = 0;
+
+	// Begin event loop; generate until none left in input file.     
+	for (int iEvent = 0; ; ++iEvent) 
+	{
+        if (!(iEvent%500)) std::cout<<"####### EVENT = " << iEvent << std::endl;	
+		// Generate events, and check whether generation failed.
+		if (!pythia.next()) 
+		{
+		// If failure because reached end of file then exit event loop.
+	    	if (pythia.info.atEndOfFile()) break; 
+		// First few failures write off as "acceptable" errors, then quit.
+			if (++iAbort < nAbort) continue;
+		    break;
+        }
+        // construct new HepMC event setting units.
+        HepMC::GenEvent* hepmcevt = new HepMC::GenEvent(HepMC::Units::GEV, HepMC::Units::MM);
+        // fill the event including PDF infos
+        ToHepMC.fill_next_event( pythia, hepmcevt );
+  
+        // Write the HepMC event to file. Done with it.
+        hepmc_file_out << hepmcevt;
+        //human_file_out << hepmcevt;
+  
+        delete hepmcevt;
+  		// End of event loop.        
+  	}                                           
+	// Give statistics. Print histogram.
+	//pythia.statistics();
+	// Done.                           
+	return 0;
+}
